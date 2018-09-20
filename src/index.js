87cf225db3deb12364a78c7697fb306446bdd7fe
Valentin Webdev <valentine.webdev@gmail.com>
2018-09-20 21:40:13 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+	let result = true;
+	let conf = bracketsConfig;
+
+	for (let i = 0; i < conf.length; i++) {		
+		if ( !((str[i] == conf[i][0]) && (str[str.length - (i + 1)] == conf[i][1])) )
+		{
+			result = false;
+		}
+	}	
+	return result;
 }
